e0d870b229fec92782fa1c1ce21bc594c8ac0292
Lukasz Sliwinski <sliwinski.lukas@gmail.com>
2014-01-15 17:20:40 +0100

    Ant enhancement - Ebean.

--- src/main/java/appFx/models/Something.java
+++ src/main/java/appFx/models/Something.java
@@ -1,8 +1,6 @@
 package appFx.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="something")
